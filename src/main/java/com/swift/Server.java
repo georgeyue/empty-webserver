706fd57fa2f42f2b8f6409ba361ce186213953c5
Jacob Plotkin <jacob.plotkin@ticketmaster.com>
2014-11-10 13:42:36 -0800

    updated with working tests

--- src/main/java/com/swift/Server.java
+++ src/main/java/com/swift/Server.java
@@ -1,9 +1,33 @@
 package com.swift;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class Server {
 
-    public static void main(String[] args)
-    {
-
+	private ServerSocket serverSocket;
+	
+	public Server(int portNumber) throws IOException {
+		serverSocket = new ServerSocket(portNumber);
+	}
+	
+    public static void main(String[] args) {
+    	int port = Integer.parseInt(args[0]);
+    	try {
+			Server myServer = new Server(port);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
+
+	public void close() {
+		// TODO Auto-generated method stub
+		try {
+			serverSocket.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
--- src/test/java/ServerShould.java
+++ src/test/java/ServerShould.java
@@ -1,13 +1,66 @@
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketAddress;
+import java.net.UnknownHostException;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import com.swift.Server;
 
 
 public class ServerShould {
 
+	int testPort;
+	Server myServer;
+	
+	@Before
+	public void setUp() {
+		testPort = 5000;
+		try {
+			myServer = new Server(testPort);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@After
+	public void tearDown() {
+		myServer.close();
+	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void listenOnPort5000() {
+		try {
+			Socket testClient = new Socket("localhost",testPort);
+			SocketAddress clientAddress = testClient.getRemoteSocketAddress();
+			int myPort = ((InetSocketAddress) clientAddress).getPort();
+			testClient.close();
+			assertEquals(5000, myPort);
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
+	@Test
+	public void shouldReturn404() throws Exception {
+		try {
+			Socket testClient = new Socket("localhost", testPort);
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
