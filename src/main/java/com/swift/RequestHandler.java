9e1616f94970b43a0be9a437f62edbafd7ce5e0e
George Yue <george.yue@ticketmaster.com>
2014-11-11 15:45:08 -0800

    remove comment

--- src/main/java/com/swift/RequestHandler.java
+++ src/main/java/com/swift/RequestHandler.java
@@ -4,12 +4,12 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-/**
- * Created by gyue on 11/11/14.
- */
 public class RequestHandler {
 
     private Response response;
+
+//    TODO let's add routes to define behaviour based on routes
+//    private Routes routes;
 
     public RequestHandler(Response response) {
         this.response = response;
