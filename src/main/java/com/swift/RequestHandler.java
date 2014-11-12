198c09e0d043582a883f0abfb840463d6d145c1e
Jacob Plotkin <jacob.plotkin@ticketmaster.com>
2014-11-12 13:03:15 -0800

    added a test for RequestHander; added fileExists method for RequestHandler class to determine existence

--- src/main/java/com/swift/RequestHandler.java
+++ src/main/java/com/swift/RequestHandler.java
@@ -3,22 +3,27 @@ package com.swift;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class RequestHandler {
 
     private Request request;
+    private String rootDirectory;
 
 //    TODO let's add routes to define behaviour based on routes
 //    private Routes routes;
 
     public RequestHandler(Request request) {
         this.request = request;
+        this.rootDirectory = System.getProperty("user.dir");
     }
 
     public void process() throws IOException {
         Response  response = request.getResponse();
         String url = request.getUrl();
-
+        
         // TODO this needs to be extracted out to have routes handle this
         if (request.getMethod().equals("GET") && url.equals("/foobar")) {
             response.setNotFoundHeader();
@@ -28,5 +33,11 @@ public class RequestHandler {
             // TODO nothing matches!
         }
         response.send();
+    }
+    
+    public boolean fileExists() {
+    	String fileToCheck = this.request.getUrl();
+    	Path pathToCheck = FileSystems.getDefault().getPath(rootDirectory, fileToCheck);
+		return Files.exists(pathToCheck);
     }
 }
--- src/test/java/com/swift/RequestHandlerTest.java
+++ src/test/java/com/swift/RequestHandlerTest.java
@@ -2,9 +2,26 @@ package com.swift;
 
 import org.junit.Test;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
+import java.nio.file.PathMatcher;
 
 import static org.junit.Assert.*;
 
 public class RequestHandlerTest {
+	RequestHandler requestHandler;
+	
+	@Test
+	public void shouldFindFilePath() throws Exception {
+        FakeSocket socket = new FakeSocket();
+        socket.setText("GET /foobar HTTP/1.1");
+		Request request = new Request(socket);
+		requestHandler = new RequestHandler(request);
+		
+		assertTrue(requestHandler.fileExists());
+	}
 }
