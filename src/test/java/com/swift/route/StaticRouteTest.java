827c0e9e1bb0e8cd32738fbbb87b9c46200d6555
Jacob Plotkin <jacob.plotkin@ticketmaster.com>
2014-11-20 16:56:19 -0800

    Updated test for StaticRoute - improved code coverage

--- src/test/java/com/swift/route/StaticRouteTest.java
+++ src/test/java/com/swift/route/StaticRouteTest.java
@@ -1,8 +1,13 @@
 package com.swift.route;
 
 import com.swift.FakeRequest;
+import com.swift.FakeSocket;
+import com.swift.router.FormRoute;
 import com.swift.router.Route;
 import com.swift.router.StaticRoute;
+import com.swift.storage.StaticStorage;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -10,22 +15,34 @@ import java.io.IOException;
 import static org.junit.Assert.*;
 
 public class StaticRouteTest {
+	FakeSocket socket;
+    FakeRequest request;
+    StaticRoute route;
 
+    @Before
+    public void setup() throws IOException {
+        socket = new FakeSocket();
+        request = new FakeRequest(socket);
+        route = new StaticRoute();
+    }
 
     @Test
     public void createNewRoute() throws IOException {
-        Route route = new StaticRoute();
         assertNotNull(route);
     }
 
     @Test
     public void testsForMatch() throws IOException {
-        FakeRequest req = new FakeRequest();
-        req.setUrl("/foobar");
-
-        Route route = new StaticRoute();
-
-        assertTrue(route.isMatch(req));
+        request.setUrl("/foobar");
+        assertTrue(route.isMatch(request));
     }
+    
+    @Test
+	public void handlesFileRequestIfMissing() throws Exception {
+		request.setUrl("/foobar");
+		route.isMatch(request);
+		route.handle();
+		assertTrue(socket.getText().contains(String.format("HTTP/1.1 404 Not Found")));
+	}
 
 }
